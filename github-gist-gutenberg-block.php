08d273195cabdb1959660ae563dcc299d41533e9
Daniel Bachhuber <daniel@handbuilt.co>
2018-01-25 14:23:20 -0800

    Initial commit

---
+++ github-gist-gutenberg-block.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     GitHub Gist Gutenberg Block
+ * Plugin URI:      https://pantheon.io
+ * Description:     Render GitHub gists
+ * Author:          Pantheon
+ * Author URI:      https://pantheon.io
+ * Text Domain:     github-gist-gutenberg-block
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Github_Gist_Gutenberg_Block
+ */
